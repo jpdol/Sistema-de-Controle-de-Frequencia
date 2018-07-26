787d2a74d814d5cf47bc004ad6aa2901a76cd78f
Jean Lima <jpdol.eng16@uea.edu.br>
2018-07-26 12:43:05 -0400

    Update SCF_backend.py

--- Aplicação/SCF_backend.py
+++ Aplicação/SCF_backend.py
@@ -13,7 +13,7 @@ def criar_conexao():
 
 class Conexao():
 	def __init__(self):
-		self.path = r"C:\Users\ADM\Desktop\SCF\DataBase"
+		self.path = r"DataBase"
 		self.conexao = sqlite3.connect(self.path+r"\SCF.db")
 		self.cursor = self.conexao.cursor()
 		self.cursor.execute("CREATE TABLE IF NOT EXISTS Laboratorio (Nome VARCHAR(45), Sigla VARCHAR(45), Logo VARCHAR(45), PRIMARY KEY(Nome, Sigla))")
@@ -38,6 +38,29 @@ def cadastrar_colaborador(tela_anterior, nome, DtNasc, Lab, Funcao, CH, DtIngres
 
 			inter.pop_up("SUCCESSFUL", "Cadastro Realizado com Sucesso")
 			inter.chamar_tela_cadastro_colaborador(tela_anterior)
+		except:
+			inter.pop_up("ERROR", "Cadastro Inválido")
+		conexao.commit()
+
+def atualizar_cadastro_colaborador(tela_anterior, nome, DtNasc, Lab, Funcao, CH, DtIngresso, status, cpf, senha, confirma_senha, foto, nome_colab):
+	if senha.get()!=confirma_senha.get():
+		inter.pop_up("ERROR", "Confirme sua Senha!")
+	else:
+		cursor = con.cursor
+		conexao = con.conexao
+
+		try:
+			if foto.get() != "":
+				file_name = ImageMethods.get_file_name(foto.get(), cpf.get())
+				foto_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "imagens","users",file_name)
+				ImageMethods.save_image(foto.get(),"users", cpf.get())
+				cursor.execute('''UPDATE Colaborador 
+					SET Nome = (?), DtNasc = (?), Lab = (?), Funcao=(?), CH=(?), DtIngresso=(?), Status=(?), cpf=(?), foto=(?), Senha=(?)''', (nome.get(), DtNasc.get(), Lab.get(), Funcao.get(), CH.get(), DtIngresso.get(), status.get(), cpf.get(), foto_path, senha.get()))
+			else:
+				cursor.execute('''UPDATE Colaborador 
+					SET Nome=(?), DtNasc=(?), Lab=(?), Funcao=(?), CH=(?), DtIngresso=(?), Status=(?), cpf=(?), foto=(?), Senha=(?)''', (nome.get(), DtNasc.get(), Lab.get(), Funcao.get(), CH.get(), DtIngresso.get(), status.get(), cpf.get(), None, senha.get()))
+
+			inter.pop_up("SUCCESSFUL", "Cadastro Atualizado com Sucesso")
 		except:
 			inter.pop_up("ERROR", "Cadastro Inválido")
 		conexao.commit()
@@ -78,6 +101,26 @@ def retorna_lista_colab(lab):
 		lista.append(str(nome[0]))
 	return lista
 
+class Colaborador():
+	def __init__(self, nome, DtNasc, Lab, Funcao, CH, DtIngresso, status, cpf, senha, foto):
+		self.nome = nome
+		self.DtNasc = DtNasc
+		self.Lab = Lab
+		self.Funcao = Funcao
+		self.CH = CH
+		self.DtIngresso = DtIngresso
+		self.status = status
+		self.cpf = cpf
+		self.senha = senha
+		self.foto = foto
+
+def retorna_colab(nome):
+	cursor = con.cursor
+	cursor.execute("SELECT * FROM Colaborador WHERE Nome = '%s'"%nome.get())
+	lista = cursor.fetchall()
+	tupla = lista[0]
+	colab = Colaborador(tupla[0], tupla[1], tupla[2], tupla[3], tupla[4], tupla[5], tupla[7], tupla[8], tupla[10], tupla[9])
+	return colab
 class ImageMethods():
 	@staticmethod
 	def get_path(line_path):
@@ -114,4 +157,11 @@ def validar_consulta(tela_anterior, lab):
 	if lab.get() != "":
 		inter.chamar_tela_consulta_2(tela_anterior, lab)
 	else:
+		inter.pop_up("ERROR", "Consulta Inválida")
+
+def validar_consulta_2(tela_anterior, nome_colab):
+	try:
+		colab = retorna_colab(nome_colab)
+		inter.chamar_tela_dados_colaborador(tela_anterior, nome_colab)
+	except:
 		inter.pop_up("ERROR", "Consulta Inválida")

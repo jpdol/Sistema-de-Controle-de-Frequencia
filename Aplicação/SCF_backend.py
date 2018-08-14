5146464f4d176b650f1790925e1dafc2545480b1
Jean Lima <jpdol.eng16@uea.edu.br>
2018-08-14 19:10:14 -0400

    Update SCF_backend.py

--- Aplicação/SCF_backend.py
+++ Aplicação/SCF_backend.py
@@ -4,6 +4,8 @@ from tkinter.filedialog import askopenfilename
 import datetime
 import SCF_interface as inter
 import sqlite3
+import tkinter.font as tkFont
+import tkinter.ttk as ttk
 
  
 user = None
@@ -335,3 +337,42 @@ def validar_consulta_2(tela_anterior, nome_colab, lab):
 	except Exception as e:
 		print(e)
 		inter.pop_up("ERROR", "Consulta Inválida")
+
+class McListBox(object):
+	def __init__(self, header, lista):
+		self.tree = None
+		self._setup_widgets(header)
+		self._build_tree(header, lista)
+
+	def _setup_widgets(self, header):
+		container = ttk.Frame()
+		container.pack(fill='both', expand=True)
+		self.tree = ttk.Treeview(columns=header, show="headings")
+		vsb = ttk.Scrollbar(orient="vertical", command=self.tree.yview)
+		hsb = ttk.Scrollbar(orient="horizontal", command=self.tree.xview)
+		self.tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+		self.tree.grid(column=0, row=0, sticky='nsew', in_=container)
+		vsb.grid(column=1, row=0, sticky='ns', in_=container)
+		hsb.grid(column=0, row=1, sticky='ew', in_=container)
+		container.grid_columnconfigure(0, weight=1)
+		container.grid_rowconfigure(0, weight=1)
+	def _build_tree(self, header, lista):
+		for col in header:
+			self.tree.heading(col, text=col.title(), command=lambda c=col: sortby(self.tree, c, 0))
+			self.tree.column(col, width=tkFont.Font().measure(col.title()))
+		for item in lista:
+			self.tree.insert('', 'end', values=item)
+			for ix, val in enumerate(item):
+				col_w = tkFont.Font().measure(val)
+				if self.tree.column(header[ix],width=None)<col_w:
+					self.tree.column(header[ix], width=col_w)
+
+
+def sortby(tree, col, descending):
+	data = [(tree.set(child, col), child) \
+	    for child in tree.get_children('')]
+	data.sort(reverse=descending)
+	for ix, item in enumerate(data):
+	    tree.move(item[1], '', ix)
+	tree.heading(col, command=lambda col=col: sortby(tree, col, \
+	    int(not descending)))

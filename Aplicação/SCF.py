c94762cbc901ab57176c8650abaff2fad6c86f99
DiegoFr75 <31371120+DiegoFr75@users.noreply.github.com>
2018-08-20 18:10:27 -0400

    Update SCF.py

--- Aplicação/SCF.py
+++ Aplicação/SCF.py
@@ -1,7 +1,8 @@
-import SCF_interface as inter
+import SCF_interface as interface
 import sqlite3
 import SCF_backend as back
 
 if __name__ == "__main__":
 	back.criar_conexao()
-	inter.chamar_tela_login()
+	app = interface.SCFapp()
+	app.mainloop()
